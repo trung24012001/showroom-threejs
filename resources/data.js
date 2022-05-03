d4fe6bd5bb34311c666f836da7d2455d22cec5c8
Vdtrung_ <hellovdtvip@gmail.com>
2022-05-04 00:30:41 +0700

    init

---
+++ resources/data.js
@@ -0,0 +1,64 @@
+const decorations = [
+  {
+    id: 1,
+    path: "./resources/showroom/",
+    name: "blender.glb",
+    content: "This is a blender",
+    scale: 2,
+    position: {
+      x: 50,
+      y: 0,
+      z: 50,
+    },
+  },
+  {
+    id: 2,
+    path: "./resources/showroom/",
+    name: "helicoper.glb",
+    content: "This is a helicoper",
+    scale: 5,
+    position: {
+      x: 50,
+      y: 0,
+      z: 100,
+    },
+  },
+  {
+    id: 3,
+    name: "mountainbike.glb",
+    path: "./resources/showroom/",
+    content: "This is a mountainbike",
+    scale: 5,
+    position: {
+      x: 50,
+      y: 0,
+      z: 150,
+    },
+  },
+  {
+    id: 4,
+    path: "./resources/showroom/",
+    name: "table.glb",
+    content: "This is a table",
+    scale: 15,
+    position: {
+      x: 50,
+      y: 0,
+      z: 200,
+    },
+  },
+  {
+    id: 5,
+    path: "./resources/showroom/",
+    name: "motorbike.glb",
+    content: "This is a motorbike",
+    scale: 10,
+    position: {
+      x: 50,
+      y: 0,
+      z: 250,
+    },
+  },
+];
+
+export { decorations };
